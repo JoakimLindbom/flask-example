a9e7510c648ea9f2119f34663762d00fa9efb154
Joakim Lindbom <Joakim.Lindbom@gmail.com>
2017-12-27 12:40:16 +0100

    indent

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
-from setuptools import setup
+#from setuptools import setup
+from distutils.core import setup
 
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='https://www.python.org/community/sigs/current/distutils-sig',
-      install_requires=['Flask>=0.7.2', 'MarkupSafe'],
-      )
+setup(
+    name='YourAppName',
+    version='1.0',
+    description='OpenShift App',
+    author='Your Name',
+    author_email='example@example.com',
+    url='https://www.python.org/community/sigs/current/distutils-sig',
+    install_requires=['Flask>=0.7.2', 'MarkupSafe'],
+    )
