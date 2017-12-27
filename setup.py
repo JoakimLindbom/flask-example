8e33cab631a83e88eea727e2fcc7d6a5112c649f
Joakim Lindbom <Joakim.Lindbom@gmail.com>
2017-12-27 12:44:59 +0100

    Requires changed

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
     author='Your Name',
     author_email='example@example.com',
     url='https://www.python.org/community/sigs/current/distutils-sig',
-    install_requires=['Flask>=0.7.2', 'MarkupSafe'],
+    requires=['Flask>=0.7.2', 'MarkupSafe'], re
     )
